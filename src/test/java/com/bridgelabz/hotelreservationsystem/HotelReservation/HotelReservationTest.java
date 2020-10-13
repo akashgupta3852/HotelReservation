b65945bb3c38617bad3e03a9ca4f9748b1de729d
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-14 02:45:15 +0530

    [Akash]Refactored: changed the test case name and added one also

--- src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservation/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/hotelreservationsystem/HotelReservation/HotelReservationTest.java
@@ -13,8 +13,21 @@ public class HotelReservationTest {
 		hotelReservation.addHotelToHotelReservation(firstHotel);
 		hotelReservation.addHotelToHotelReservation(secondHotel);
 		hotelReservation.addHotelToHotelReservation(thirdHotel);
+		String hotelName = hotelReservation.findTheCheapestBestRatedHotel();
+		Assert.assertEquals("Bridgewood", hotelName);
+	}
+	
+
+	@Test
+	public void givenHotelsWhenAddedShouldReturnTheBestRatedHotel() {
+		Hotel firstHotel = new Hotel("Lakewood", 110, 90, 3, "11Sep2020", "12Sep2020");
+		Hotel secondHotel = new Hotel("Bridgewood", 150, 50, 4, "11Sep2020", "12Sep2020");
+		Hotel thirdHotel = new Hotel("Ridgewood", 220, 150, 5, "11Sep2020", "12Sep2020");
+		HotelReservation hotelReservation = new HotelReservation();
+		hotelReservation.addHotelToHotelReservation(firstHotel);
+		hotelReservation.addHotelToHotelReservation(secondHotel);
+		hotelReservation.addHotelToHotelReservation(thirdHotel);
 		String hotelName = hotelReservation.findBestRatedHotel();
-		System.out.println(hotelName);
 		Assert.assertEquals("Ridgewood", hotelName);
 	}
 }

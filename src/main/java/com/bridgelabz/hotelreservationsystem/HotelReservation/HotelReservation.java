48a6b6777a844b09d30b8b73ed5be150aeb529b9
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-13 22:54:09 +0530

    [Akash]Added: Welcome Message

---
+++ src/main/java/com/bridgelabz/hotelreservationsystem/HotelReservation/HotelReservation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hotelreservationsystem.HotelReservation;
+
+public class HotelReservation {
+	public void printWelcome() {
+		System.out.println("Welcome to Hotel Reservation Program");
+	}
+}
